4fe0e914f76f8171be248e79702e40aa81a37eaf
JP Richardson <jprichardson@gmail.com>
2015-07-14 09:50:25 -0500

    JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function isRenderer () {
   // node-integration is disabled
-  if (!process) return true;
-  
+  if (!process) return true
+
   // We're in node.js somehow
-  if (!process.type) return false;
-  
-  return process.type === 'renderer';
+  if (!process.type) return false
+
+  return process.type === 'renderer'
 }
 
 module.exports = isRenderer()
